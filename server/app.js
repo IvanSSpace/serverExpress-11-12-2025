bd7b06659b761b20d68c9fffadc634c61f2879b6
Ivan MacOS <oldrum75@gmail.com>
2025-12-12 19:26:44 +0600

    Add dotenv and bcrypt for environment configuration and password hashing
    - Updated .gitignore to include .env file.
    - Added bcrypt and dotenv as dependencies in package.json and package-lock.json.
    - Configured dotenv in app.js for environment variable management.
    - Modified database connection in db.js to use environment variables.
    - Implemented user registration endpoint in app.js with password hashing.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const { loggerMiddleware } = require('./middleware.js');
 const { pool } = require('./db.js');
 const app = express();
-const port = 3000;
+const bcrypt = require('bcrypt');
+
+// const someOtherPlaintextPassword = 'not_bacon';
 
 // const users = [
 //   { id: 0, name: 'vlad', age: 31, sex: 'man' },
@@ -23,7 +26,7 @@ app.get('/ping', (req, res) => {
 });
 
 app.get('/users', async (req, res) => {
-  const data = await pool.query('SELECT * FROM users');
+  const data = await pool.query('SELECT * FROM users;');
   res.json(data.rows);
 });
 
@@ -33,7 +36,7 @@ app.get('/users/:id', async (req, res) => {
   // if (isNaN(id)) res.status(400).json({ error: 'invalid id' });
 
   const data = await pool.query(
-    `SELECT id, email, name, created_at  FROM users WHERE id = $1 LIMIT 1`,
+    `SELECT id, email, name, created_at  FROM users WHERE id = $1 LIMIT 1;`,
     [id]
   );
 
@@ -42,6 +45,37 @@ app.get('/users/:id', async (req, res) => {
   res.json(data.rows[0]);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.post('/auth/register', async (req, res) => {
+  const { name, email, password } = req.body;
+  const emailIsBusy = await pool.query(
+    `SELECT id FROM users WHERE email = $1;
+`,
+    [email]
+  );
+
+  if (emailIsBusy.rowCount) {
+    return res.status(400).json({ error: 'email is busy' });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+
+  bcrypt.hash(password + process.env.PEPPER_SECRET, 10, async (err, password_hash) => {
+    const response = await pool.query(
+      `INSERT INTO users (name, email, password_hash)
+  VALUES ($1, $2, $3)
+  RETURNING id, name, email, created_at;`,
+      [name, email, password_hash]
+    );
+    res.status(200).json({ message: response.rows[0] });
+  });
+});
+
+// app.post('/login', async (req, res) => {
+//   // 1 клиент присылает email password а сервер ищет пользователя по email
+//   // 2 сравниваем пароль с password_hash
+//   // 3 если совпало → создаём JWT
+// });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
--- server/db.js
+++ server/db.js
@@ -1,12 +1,13 @@
+import 'dotenv/config';
 import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: 'shar',
-  host: 'localhost',
-  database: 'serverexpress11122025',
-  password: '',
-  port: 5432,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export { pool };
